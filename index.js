b03cce5b2dc2b6934ad76d893cdf32ce0100694a
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-03-11 23:38:13 -0600

    Logic creates the table but doens't earse it or change color

--- index.js
+++ index.js
@@ -5,19 +5,36 @@ let gridSquares = 0;
 
 function createDiv(i) {
     newDiv = document.createElement('div')
-    newDiv.setAttribute('id',`newDiv${i}`)
-    mainContainer.append(newDiv)
+    newDiv.setAttribute('id',`newDiv`)
+}
+
+function createRows(numOfRows) {
+    for (let i = 0; i<numOfRows -1;i++) {
+        subContainer = document.createElement('div')
+        subContainer.setAttribute('id', 'sub-container')
+        body.append(subContainer)
+        for ( let x = 0;x<numOfRows;x++) {
+            createDiv(numOfRows)
+            subContainer.append(newDiv)
+        }
+    }
+}
+
+function generateSquares() {
+    if (gridSquares > 0 && gridSquares < 101) {
+        for (let i = 0; i<gridSquares;i++) {
+            createDiv(i)
+            mainContainer.append(newDiv)
+        }
+        createRows(gridSquares)
+    } else {
+        console.log("FALSE")
+    }
 }
 
 gridSelectionBTN.addEventListener('click', function getSquares() {
     gridSquares = 0
     gridSquares = parseInt(prompt("How many squares?","0"),10)
-    if (gridSquares > 0 && gridSquares < 101) {
-        for (let i = 0; i<gridSquares;i++) {
-            console.log('X')
-            createDiv(i)
-        }
-    } else {
-        console.log("FALSE")
-    }
+    generateSquares()
 })
+
