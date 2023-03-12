362148f464428969a91eb4d14403475eed0799e0
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-03-12 12:41:02 -0500

    Bottom square works more responsively

--- index.js
+++ index.js
@@ -4,40 +4,40 @@ const gridSelectionBTN = document.getElementById('grid-selection')
 let gridSquares = 0;
 
 function createDiv(i) {
-    newDiv = document.createElement('div')
-    newDiv.setAttribute('id','newDiv')
+  newDiv = document.createElement('div')
+  newDiv.setAttribute('id', 'newDiv')
 }
 
 function createRows(numOfRows) {
-    for (let i = 0; i<numOfRows -1;i++) {
-        subContainer = document.createElement('div')
-        subContainer.setAttribute('id', 'container')
-        body.append(subContainer)
-        for ( let x = 0;x<numOfRows;x++) {
-            createDiv(numOfRows)
-            subContainer.append(newDiv)
-        }
+  for (let i = 0; i < numOfRows - 1; i++) {
+    subContainer = document.createElement('div')
+    subContainer.setAttribute('id', 'container')
+    body.append(subContainer)
+    for (let x = 0; x < numOfRows; x++) {
+      createDiv(numOfRows)
+      subContainer.append(newDiv)
     }
+  }
 }
 
 function generateSquares() {
-    if (gridSquares > 0 && gridSquares < 101) {
-        for (let i = 0; i<gridSquares;i++) {
-            createDiv(i)
-            mainContainer.append(newDiv)
-        }
-        createRows(gridSquares)
-        newDiv.addEventListener('mouseover', function log() {
-            for (let i = 0; i<gridSquares;i++)
-            newDiv.style.backgroundColor = "black"
-        })
-    } else {
-        console.log("FALSE")
+  if (gridSquares > 0 && gridSquares < 101) {
+    for (let i = 0; i < gridSquares; i++) {
+      createDiv()
+      mainContainer.append(newDiv)
     }
+    createRows(gridSquares)
+    newDiv.addEventListener('mouseover', function log() {
+    newDiv.style.backgroundColor = "black"
+    })
+  } else {
+    console.log("FALSE")
+  }
+
 }
 
 gridSelectionBTN.addEventListener('click', function getSquares() {
-    gridSquares = 0
-    gridSquares = parseInt(prompt("How many squares?","0"),10)
-    generateSquares()
+  gridSquares = 0
+  gridSquares = parseInt(prompt("How many squares?", "0"), 10)
+  generateSquares()
 })
