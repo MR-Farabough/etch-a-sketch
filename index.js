ae9fcf490676e3d940bdc0f58419b78964cc03bc
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-03-11 21:49:30 -0600

    Working on the logic it will only add boxes doesn't reset

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 let mainContainer = document.getElementById('main-container')
+let body = document.querySelector("body")
 const gridSelectionBTN = document.getElementById('grid-selection')
 let gridSquares = 0;
 
+function createDiv(i) {
+    newDiv = document.createElement('div')
+    newDiv.setAttribute('id',`newDiv${i}`)
+    mainContainer.append(newDiv)
+}
+
 gridSelectionBTN.addEventListener('click', function getSquares() {
-    gridSquares = prompt("How many squares?")
+    gridSquares = 0
+    gridSquares = parseInt(prompt("How many squares?","0"),10)
+    if (gridSquares > 0 && gridSquares < 101) {
+        for (let i = 0; i<gridSquares;i++) {
+            console.log('X')
+            createDiv(i)
+        }
+    } else {
+        console.log("FALSE")
+    }
 })
