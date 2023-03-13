d9d74b69211841652b2d16a8ef73165511424c37
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-03-12 22:21:00 -0500

    Recieved help and completed project

--- index.js
+++ index.js
@@ -1,43 +1,34 @@
-let mainContainer = document.getElementById('container')
+let mainContainer = document.getElementById('main-container')
 let body = document.querySelector("body")
 const gridSelectionBTN = document.getElementById('grid-selection')
 let gridSquares = 0;
 
-function createDiv(i) {
+function createDiv() {
   newDiv = document.createElement('div')
-  newDiv.setAttribute('id', 'newDiv')
+  newDiv.setAttribute('class', 'square')
 }
 
-function createRows(numOfRows) {
-  for (let i = 0; i < numOfRows - 1; i++) {
-    subContainer = document.createElement('div')
-    subContainer.setAttribute('id', 'container')
-    body.append(subContainer)
-    for (let x = 0; x < numOfRows; x++) {
-      createDiv(numOfRows)
-      subContainer.append(newDiv)
-    }
-  }
-}
-
-function generateSquares() {
-  if (gridSquares > 0 && gridSquares < 101) {
-    for (let i = 0; i < gridSquares; i++) {
+function generateSquares(gridMultiplied) {
+    for (let i = 0; i < gridMultiplied; i++) {
       createDiv()
       mainContainer.append(newDiv)
     }
-    createRows(gridSquares)
-    newDiv.addEventListener('mouseover', function log() {
-    newDiv.style.backgroundColor = "black"
-    })
-  } else {
-    console.log("FALSE")
+    
   }
-
-}
-
 gridSelectionBTN.addEventListener('click', function getSquares() {
-  gridSquares = 0
-  gridSquares = parseInt(prompt("How many squares?", "0"), 10)
-  generateSquares()
+  mainContainer.innerText = ""
+  // gridSquares = parseInt(prompt("How many squares?", "0"), 10)
+  gridSquares = 18 * 18
+  // TODO HANDLE SITUATION WHERE INPUT IS TO LARGE OR NOT A NUMBER
+  const gridMultiplied = gridSquares
+  generateSquares(gridMultiplied)
+
+  const divs = document.querySelectorAll('.square')
+  for (let index = 0; index < divs.length; index++) {
+    const divEL = divs[index]
+    divEL.addEventListener('mouseover', function log() {
+      divEL.style.backgroundColor = "black"
+    })
+  }
 })
+
